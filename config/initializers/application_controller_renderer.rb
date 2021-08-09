7b8322f3b52b6a8491a5dc4aabee820cb485a457
nori1jz <apriotype2xxnorixx@gmail.com>
2021-08-09 11:11:56 +0000

    [Finish]アプリケーションを作ろう2-3章

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '33ec0de22ae8cd67db7ba7740ac2cf822bfec6bde4715bf88edd5f2a47a656b32b002609ea2ef45bb044c46bc53addc2a4fadcb4d0f142dfb7c937a714f39400'
