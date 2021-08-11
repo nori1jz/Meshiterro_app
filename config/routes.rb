a4518834476c81ee5b8f42869c74ecd70ade3645
nori1jz <apriotype2xxnorixx@gmail.com>
2021-08-11 13:56:48 +0000

    [Finish]アプリケーションを作ろう2-6章

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+  resources :post_images, only: [:new, :create, :index, :show] do
     resource :favorites, only: [:create, :destroy]
 
     resources :post_comments, only: [:create, :destroy]
--- db/migrate/20210810162718_addprofile_id_to_users.rb
+++
@@ -1,4 +0,0 @@
-class AddprofileIdToUsers < ActiveRecord::Migration[5.2]
-  def change
-  end
-end
